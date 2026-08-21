e34d2a69955593ccfa07f1499a878fd6cc0a2cb4
LokeshGopal2104 <lokeshgopalmeka196@gmail.com>
2026-08-21 11:25:17 +0530

    Time: 1 ms (99.85%), Space: 46.4 MB (82.05%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,82 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<>();
+
+        boolean [] cols = new boolean[n];
+
+        boolean [] diag1 = new boolean[2*n-1];
+        boolean [] diag2 = new boolean[2*n-1];
+
+        char [][] board = new char[n][n];
+
+        for ( int row = 0; row < n; row++){
+            Arrays.fill(board[row],'.');
+        }
+
+        backtrack(
+            0,
+            n,
+            board,
+            cols,
+            diag1,
+            diag2,
+            res
+        );
+
+        return res;
+    }
+
+    public void backtrack(int row,
+                int n ,
+                char [][] board,
+                boolean [] cols,
+                boolean [] diag1,
+                boolean [] diag2,
+                List<List<String>> res){
+
+                    if(row == n){
+
+                        List<String> solution = new ArrayList<>();
+
+                        for ( int i = 0; i < n; i++){
+                            solution.add(new String(board[i]));
+                        }
+
+                        res.add(solution);
+                        return;
+                    }
+
+                    for( int col = 0; col < n; col++){
+                        int d1 = row - col + n - 1;
+                        int d2 = row  + col;
+
+                        if(cols[col] || diag1[d1] || diag2[d2]){
+                            continue;
+                        }
+
+                        board[row][col] = 'Q';
+
+                        cols[col] = true;
+                        diag1[d1] = true;
+                        diag2[d2] = true;
+
+                        backtrack(
+                            row+1,
+                            n,
+                            board,
+                            cols,
+                            diag1,
+                            diag2,
+                            res
+                        );
+
+                        board[row][col] = '.';
+
+                        cols[col] = false;
+                        diag1[d1] = false;
+                        diag2[d2] = false;
+                    }
+
+                
+    }
+}
